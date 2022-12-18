7f5ec66bcff37a4e7e678dcfe1305835f90e1b27
Sapna Mehra <mehrasapna240@gmail.com>
2022-12-17 21:27:04 -0800

    Initial Author Router

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ if(process.env.NODE_ENV !=='production'){
 const express = require('express')
 const app=express()
 const expressLayouts = require('express-ejs-layouts')
+
 const indexRouter=require('./routes/index')
+
+const authorsRouter =require('./routes/authors')
+const bodyParser = require('body-parser')
 
 app.set('view engine' , 'ejs' )
 app.set('views' ,__dirname + '/views' )
 app.set('layout' , 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 
 const mongoose=require('mongoose')
 mongoose.set("strictQuery", false);
@@ -24,4 +29,5 @@ db.once('error' , () =>console.log("connected to mongoose"))
 
 
 app.use('/' , indexRouter)
+app.use('/authors' , authorsRouter)
 app.listen(process.env.PORT || 3000)
